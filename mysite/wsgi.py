4712fd5e7441f6ce02d94afe08408123f4dee5ea
harshit-j <harsh35plus@gmail.com>
2017-09-02 13:40:44 +0530

    Added files for Heroku deployment

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite3 project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite3.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
 application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
